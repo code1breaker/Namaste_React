9f0a5164cf26aea879a0c1420de881f91b049d2b
code1breaker <115928576+code1breaker@users.noreply.github.com>
2023-02-11 08:14:23 +0530

    Update Body.jsx

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -7,25 +7,28 @@ import RestaurantCard from "../RestaurantCard/RestaurantCard";
 import { FETCH_URL } from "../../utils/constant";
 
 import useRestaurant from "../../utils/useRestaurant";
+import HomeShimmer from "../Shimmer/HomeShimmer";
 
 const Body = () => {
   const restaurant = useRestaurant(FETCH_URL);
   const [getRestaurant, setGetRestaurant] = useState(restaurant);
   // console.log(getRestaurant);
 
-  return (
+  return !getRestaurant ? (
+    <HomeShimmer />
+  ) : (
     <>
+      {console.log("getRestaurant", getRestaurant)}
       <section className="carousel">
         <Carousel restaurant={restaurant} />
       </section>
-
       <Search setGetRestaurant={setGetRestaurant} />
-
       <section className="container flex restaurant">
-        <RestaurantCard getRestaurant={getRestaurant} />
+        <RestaurantCard restaurant={getRestaurant} />
       </section>
     </>
   );
+  // );
 };
 
 export default Body;

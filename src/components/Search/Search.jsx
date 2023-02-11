1678d092553b67e3066c1a9cdd96a11840abda97
code1breaker <115928576+code1breaker@users.noreply.github.com>
2023-02-11 08:15:35 +0530

    Update Search.jsx

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./Search.css";
 
 import { FETCH_URL } from "../../utils/constant";
@@ -7,21 +7,26 @@ import useRestaurant from "../../utils/useRestaurant";
 const Search = ({ setGetRestaurant }) => {
   const restaurant = useRestaurant(FETCH_URL);
   const [inputValue, setInputValue] = useState("");
+  const [allRestaurant, setAllRestaurant] = useState([]);
+  const [filterRestaurant, setFilterRestaurant] = useState([]);
 
-  const [allRestaurant] = useState(
-    restaurant?.data?.cards[2]?.data?.data?.cards
-  );
-  const [filterRestaurant, setFilterRestaurant] = useState(
-    restaurant?.data?.cards[2]?.data?.data?.cards
-  );
-
-  const filter = () => {
-    const filterData = allRestaurant?.filter((restaurant) => {
-      restaurant?.data?.name?.toLowerCase()?.includes(inputValue.toLowerCase());
-
-      setFilterRestaurant(filterData);
+  useEffect(() => {
+    setTimeout(() => {
+      setAllRestaurant(restaurant?.data?.cards[2]?.data?.data?.cards);
+      setFilterRestaurant(restaurant?.data?.cards[2]?.data?.data?.cards);
       setGetRestaurant(filterRestaurant);
+    }, 1000);
+    // console.log("allrestaurant", allRestaurant);
+    // console.log("filterRestaurant", filterRestaurant);
+    // console.log("restaurant", restaurant);
+  }, []);
+
+  const filter = (allRestaurant, inputValue) => {
+    const data = allRestaurant?.filter((restaurant) => {
+      restaurant?.data?.name.toLowerCase().includes(inputValue.toLowerCase());
     });
+    console.log(data);
+    return data;
   };
 
   const handleInput = (e) => {
@@ -36,10 +41,19 @@ const Search = ({ setGetRestaurant }) => {
           value={inputValue}
           onChange={handleInput}
         />
-        <button onClick={filter}>Search</button>
+        <button
+          onClick={() => {
+            const data = filter(allRestaurant, inputValue);
+            setFilterRestaurant(data);
+          }}
+        >
+          Search
+        </button>
       </div>
     </>
   );
 };
 
 export default Search;
+
+// restaurant?.data?.cards[2]?.data?.data?.cards
